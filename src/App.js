20d6874d213bcb0b0c498ca79dbfb067c25779a4
Athul SS <athul822@gmail.com>
2022-03-08 21:36:07 +0530

    input field state updated to show the text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [toDos, setTodos] = useState([])
+  const [toDo, setTodo] = useState('')
     return (
         <div className="container">
             <div className="header">
                 <h1 className="title">To-Do-App</h1>
                 <h3 className="sub-title">To Do List</h3>
             </div>
-            
+
             <div className="input">
-                
-                <input className="todo-input" type="text" placeholder="🖊️ Add item..." />
+                <input value ={toDo} className="todo-input" onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
                 <i className="fas fa-plus add-btn"></i>
+            </div>
+            <div className="todos">
+                <div className="todo">
+                  <div className="left">
+                    <input type="checkbox" name="" id="" />
+                    <p>Rect tutorial</p>
+                    </div>
+                    <div className="right">
+                    <i className="fas fa-times"></i>
+                    </div>
+                </div>
             </div>
         </div>
     );
